56bcb15ab3afa7709fb8e9789daaee221c6a28b6
shengliang <ahliang51@hotmail.com>
2020-05-12 22:48:54 +0800

    updated register component

--- web-frontend/src/components/Register/Register.js
+++ web-frontend/src/components/Register/Register.js
@@ -1,151 +1,178 @@
-import React, { Component, useState } from "react";
+import React, { Component, } from "react";
 import {
-  InputGroup,
-  Container,
-  Row,
-  Image,
-  Button,
-  Col,
   Form,
-} from "react-bootstrap";
+  Input,
+  Select,
+  Button,
+} from 'antd';
 import "./Register.scss";
 import Logo from "../Logo/Logo";
 
-function FormExample() {
+const { Option } = Select;
 
-  const [validated, setValidated] = useState(false);
-  const [cpassword, currentPass] = useState("");
-  const [confirmpassword, confPass] = useState("");
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-    if (confirmpassword != cpassword) {
-      alert("Passwords do not match");
-      event.preventDefault();
-      event.stopPropagation();
-    }
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
 
-    setValidated(true);
-  };
-
-
-
-  return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
-      <Form.Row>
-        <Form.Group as={Col} md="6" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="First name"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="6" controlId="validationCustom02">
-          <Form.Label>Last name</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Last name"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-      </Form.Row>
-      <Form.Row>
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            required
-            type="email"
-            placeholder="Email Address"
-          />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid email address.
-            </Form.Control.Feedback>
-        </Form.Group>
-
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>Trading Experience</Form.Label>
-          <Form.Control as="select" value="Choose...">
-            <option>Beginner</option>
-            <option>Less than 5 years</option>
-            <option>5 to 10 years</option>
-            <option>More than 10 years</option>
-          </Form.Control>
-        </Form.Group>
-
-        <Form.Group as={Col} md="4" controlId="formGridState">
-          <Form.Label>Risk Appetite</Form.Label>
-          <Form.Control as="select" value="Choose...">
-            <option>Giam Siap</option>
-            <option>Okay lah</option>
-            <option>Lamborghini money</option>
-          </Form.Control>
-        </Form.Group>
-
-      </Form.Row>
-
-      <Form.Row>
-        <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="password" required
-
-            onChange={(event) => currentPass(event.target.value)}
-
-          />
-          <Form.Control.Feedback type="invalid">
-            Please provide a password.
-            </Form.Control.Feedback>
-        </Form.Group>
-
-        <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>Confirm password</Form.Label>
-          <Form.Control type="password"
-            placeholder="password" required
-            onChange={(event) => confPass(event.target.value)}
-          />
-          <Form.Control.Feedback type="invalid">
-            Please provide a password.
-            </Form.Control.Feedback>
-        </Form.Group>
-
-      </Form.Row>
-
-      <Container>
-        <Row>
-          <Col>
-            <div className="back-btn">
-              <Button variant="dark" size="lg" href="/">Back</Button>
-            </div>
-          </Col>
-          <Col>
-            <div className="submit-btn">
-              <Button type="submit" size="lg">Register</Button>
-            </div>
-          </Col>
-
-        </Row>
-      </Container>
-
-    </Form>
-  );
-}
-
-
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      offset: 8,
+    },
+  },
+};
 
 class Register extends Component {
+
+
   render() {
+
+    let onFinish = values => {
+      console.log('Received values of form: ', values);
+    };
+
     return (
       <div className="Register">
         <Logo></Logo>
-        <FormExample></FormExample>
-      </div>
+        <Form {...formItemLayout} >
+          <Form.Item
+            name="firstName"
+            label="First Name"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your first name',
+              },
+            ]}
+            hasFeedback
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="lastName"
+            label="Last Name"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your last name',
+              },
+            ]}
+            hasFeedback
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="email"
+            label="E-mail"
+            rules={[
+              {
+                type: 'email',
+                message: 'The input is not valid E-mail!',
+              },
+              {
+                required: true,
+                message: 'Please input your E-mail!',
+              },
+            ]}
+            hasFeedback
+
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="tradingExperience"
+            label="Trading Experience"
+            rules={[
+              {
+                required: true,
+                message: 'Please select your experience',
+              },
+            ]}
+            hasFeedback
+          >
+            <Select placeholder="Select Experience">
+              <Option value="Beginner">Beginner</Option>
+              <Option value="lessThan5">Less than 5 years</Option>
+              <Option value="5to10">5 to 10 years</Option>
+              <Option value="moreThan10">More than 10 years</Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            name="riskAppetite"
+            label="Risk Appetite"
+            rules={[
+              {
+                required: true,
+                message: 'Please select your risk appetite',
+              },
+            ]}
+            hasFeedback
+          >
+            <Select placeholder="Select Risk Appetite">
+              <Option value="giamSiap">Giam Siap</Option>
+              <Option value="okay">Okay Lah</Option>
+              <Option value="lamborgini">Lamborgini here i come</Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="confirm"
+            label="Confirm Password"
+            dependencies={['password']}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: 'Please confirm your password!',
+              },
+              ({ getFieldValue }) => ({
+                validator(rule, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject('The two passwords that you entered do not match!');
+                },
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form.Item>
+
+        </Form>
+      </div >
 
 
     );
