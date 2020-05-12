99f224c46c27e648c6930af830e09a34b4b51926
shengliang <ahliang51@hotmail.com>
2020-05-12 17:01:32 +0800

    added navigation for different menus

--- web-frontend/src/components/Home/SideMenu/SideMenu.js
+++ web-frontend/src/components/Home/SideMenu/SideMenu.js
@@ -18,6 +18,7 @@ const { SubMenu } = Menu;
 class SideMenu extends Component {
     state = {
         collapsed: false,
+        defaultKey: 1
     };
 
     onCollapse = collapsed => {
@@ -25,16 +26,28 @@ class SideMenu extends Component {
         this.setState({ collapsed });
     };
 
+    onClickMenu = id => {
+        this.setState({
+            ...this.state, defaultKey: id
+        })
+    }
+
     render() {
+
+        let components = (<Dashboard></Dashboard>);
+        if (this.state.defaultKey == 2) {
+            components = (<Trade></Trade>)
+        }
+
         return (
             <Layout style={{ minHeight: '100vh' }}>
                 <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
                     <div className="side-menu"></div>
                     <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                        <Menu.Item key="1" icon={<HomeOutlined />}>
+                        <Menu.Item key="1" icon={<HomeOutlined />} onClick={() => this.onClickMenu("1")}>
                             Dashboard
                          </Menu.Item>
-                        <Menu.Item key="2" icon={<LineChartOutlined />}>
+                        <Menu.Item key="2" icon={<LineChartOutlined />} onClick={() => this.onClickMenu("2")}>
                             Trade
                         </Menu.Item>
                         <Menu.Item key="3" icon={<HistoryOutlined />}>
@@ -58,8 +71,7 @@ class SideMenu extends Component {
                             {/* <Breadcrumb.Item>Bill</Breadcrumb.Item> */}
                         </Breadcrumb>
                         <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                            <Dashboard></Dashboard>
-                            {/* <Trade></Trade> */}
+                            {components}
                         </div>
                     </Content>
                     {/* <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer> */}
