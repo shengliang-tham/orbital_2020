b0232bb9c89e60ea608ad23a23a1e95fa0adb15f
shengliang <ahliang51@hotmail.com>
2020-05-28 16:07:22 +0800

    added environment variables

--- web-app/client/config/webpack.config.js
+++ web-app/client/config/webpack.config.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const dotenv = require('dotenv');
 const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
@@ -56,8 +57,38 @@ const sassModuleRegex = /\.module\.(scss|sass)$/;
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
 module.exports = function (webpackEnv) {
+
+  // Get the root path (assuming your webpack config is in the root of your project!)
+  const currentPath = path.join(__dirname);
+
+  // Create the fallback path (the production .env)
+  const basePath = currentPath + '/.env';
+
+  // We're concatenating the environment name to our filename to specify the correct env file!
+  const envPath = basePath + '.' + webpackEnv;
+
+  // Check if the file exists, otherwise fall back to the production .env
+  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+
+
+  // Set the path parameter in the dotenv config
+  const fileEnv = dotenv.config({ path: finalPath }).parsed;
+
+  console.log("hello")
+  console.log(finalPath)
+
+
+  // reduce it to a nice object, the same as before (but with the variables from the file)
+  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+    return prev;
+  }, {});
+
   const isEnvDevelopment = webpackEnv === 'development';
   const isEnvProduction = webpackEnv === 'production';
+
+
+
 
   // Variable used for enabling profiling in Production
   // passed into alias object. Uses a flag if passed into the build command
@@ -560,6 +591,7 @@ module.exports = function (webpackEnv) {
       ],
     },
     plugins: [
+      new webpack.DefinePlugin(envKeys),
       // Generates an `index.html` file with the <script> injected.
       new HtmlWebpackPlugin(
         Object.assign(
--- web-app/client/src/global-variables.js
+++ web-app/client/src/global-variables.js
@@ -1,1 +1,4 @@
-export const backendUrl = "http://localhost:5000";
+
+// export const backendUrl = "http://localhost:5000";
+// export const backendUrl = "https://orbital-2020.herokuapp.com/";
+export const backendUrl = process.env.BACKEND_API_URL;
