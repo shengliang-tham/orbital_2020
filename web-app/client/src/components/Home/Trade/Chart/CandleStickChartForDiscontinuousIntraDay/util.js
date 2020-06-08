e6b3205b1273a7e194ff6fa3fd2b5d929bb883c7
shengliang <ahliang51@hotmail.com>
2020-06-08 15:54:32 +0800

    added separate chart for intra daay trading and connect to trading api

---
+++ web-app/client/src/components/Home/Trade/Chart/CandleStickChartForDiscontinuousIntraDay/util.js
@@ -0,0 +1,45 @@
+
+
+import { tsvParse, csvParse } from "d3-dsv";
+import { timeParse } from "d3-time-format";
+import axios from 'axios'
+
+function parseData(parse) {
+    return function (d) {
+        d.date = parse(d.date);
+        d.open = +d.open;
+        d.high = +d.high;
+        d.low = +d.low;
+        d.close = +d.close;
+        d.volume = +d.volume;
+
+        return d;
+    };
+}
+
+const parseDateTime = timeParse("%Y-%m-%d %H:%M:%S");
+
+export function getData() {
+    // const promiseIntraDayContinuous = fetch("https://cdn.rawgit.com/rrag/react-stockcharts/master/docs/data/bitfinex_xbtusd_1m.csv")
+    //     .then(response => response.text())
+    //     .then(data => csvParse(data, parseData(parseDateTime)))
+    //     .then(data => {
+    //         data.sort((a, b) => {
+    //             return a.date.valueOf() - b.date.valueOf();
+    //         });
+    //         return data;
+    //     });
+    // return promiseIntraDayContinuous;
+
+    // const promiseMSFT = fetch("http://localhost:8000/api/stockOHLC?ticker=Z74.SI&interval=D&startDate=1/06/2020&endDate=5/06/2020")
+    //     .then(response => response.text())
+    //     .then(data => console.log(data))
+    //     .then(data => csvParse(data, parseData(parseDateTime)))
+    // return promiseMSFT;
+
+    const promiseMSFT = axios("http://localhost:8000/api/stockOHLC?ticker=Z74.SI&interval=1&startDate=1/06/2020&endDate=5/06/2020")
+        // .then(response => response.text())
+        // .then(data => console.log(data))
+        .then(data => csvParse(data.data, parseData(parseDateTime)))
+    return promiseMSFT;
+}   
--- web-app/client/src/components/Home/Trade/Trade.js
+++ web-app/client/src/components/Home/Trade/Trade.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import {render} from 'react-dom';
-import Chart from './chart';
-import {getData} from './utils';
-import {TypeChooser} from "react-stockcharts/lib/helper";
+import { render } from 'react-dom';
+// import Chart from './chart';
+// import { getData } from './utils';
+import { TypeChooser } from "react-stockcharts/lib/helper";
+import CandleStickChart from './Chart/Candlestick/CandleStickChart';
+import { getData } from './Chart/CandleStickChartForDiscontinuousIntraDay/util';
+import CandleStickChartForDiscontinuousIntraDay from './Chart/CandleStickChartForDiscontinuousIntraDay/CandleStickChartForDiscontinuousIntraDayCandleStickChartForDiscontinuousIntraDay';
+
 
 class Trade extends React.Component {
-    componentDidMount() {
-        getData().then(data => {
-          this.setState({ data })
-        })
-      }
-      render() {
-        if (this.state == null) {
-          return <div>Loading...</div>
-        }
-        return (
-          <TypeChooser>
-            {type => <Chart type={type} data={this.state.data} />}
-          </TypeChooser>
-        )
-      }
+
+
+
+  componentDidMount() {
+    getData().then(data => {
+      console.log(data)
+      this.setState({ data })
+    })
+  }
+  render() {
+    if (this.state == null) {
+      return <div>Loading...</div>
+    }
+    return (
+      <TypeChooser>
+        {type => <CandleStickChartForDiscontinuousIntraDay type={type} data={this.state.data} />}
+      </TypeChooser>
+    )
+  }
 }
 
 export default Trade;
