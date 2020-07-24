9039542c09b99b74c003abd7e5b5bfe26c8a4e9f
lundypgk <LundyPGK@gmail.com>
2020-07-24 18:20:30 +0800

    changed stock ohlc layout

--- TradingAPI/api/package/stockOHLC.py
+++ TradingAPI/api/package/stockOHLC.py
@@ -40,40 +40,41 @@ def stockOHLC(bar_data, token):
         t_End = str(int(time.time()))
     else:
         t_End = str(date_Unix(bar['endDate'])+4314000)
-    URL = 'https://finnhub.io/api/v1/stock/candle?symbol='+Symbol + \
+    URL2 = 'https://finnhub.io/api/v1/stock/candle?symbol='+Symbol + \
         '&resolution='+resolution+'&from='+t_Start+'&to='+t_End+'&token='+token
     # print(url)
+    URL= 'https://finnhub.io/api/v1/quote?symbol='+Symbol+'&token='+token
 
     r = requests.get(URL)
     r_json = r.json()
     try:
-        r_Open = np.array(r_json['o'])
-        r_High = np.array(r_json['h'])
-        r_Low = np.array(r_json['l'])
-        r_Close = np.array(r_json['c'])
-        r_time = np.array(r_json['t'])
-        df2 = pd.DataFrame(r_time, columns=['Time'])
-        df2['date'] = df2['Time'].apply(lambda x: unix_Date(x))
-        r_vol = np.array(r_json['v'])
-        df = pd.DataFrame(r_Open, columns=['open'])
-        df['High'] = r_High
-        df['Low'] = r_Low
-        df['Close'] = r_Close
-        df['Volume'] = r_vol
-        df['Date'] = df2['date']
-        df.columns = ['open', 'high', 'low', 'close', 'volume', 'date']
-        df['RSI'] = RSI(df, 14)
-        # MACD
-        # df = MACD(df)
-        df["MA_20"] = EMA(df, 20)
-        df["ADX"] = ADX(df, 20)
-        df["OBV"] = OBV(df)
-        df["slope"] = slope(df)
-
+        # r_Open = np.array(r_json['o'])
+        # r_High = np.array(r_json['h'])
+        # r_Low = np.array(r_json['l'])
+        # r_Close = np.array(r_json['c'])
+        # r_time = np.array(r_json['t'])
+        # df2 = pd.DataFrame(r_time, columns=['Time'])
+        # df2['date'] = df2['Time'].apply(lambda x: unix_Date(x))
+        # r_vol = np.array(r_json['v'])
+        # df = pd.DataFrame(r_Open, columns=['open'])
+        # df['High'] = r_High
+        # df['Low'] = r_Low
+        # df['Close'] = r_Close
+        # df['Volume'] = r_vol
+        # df['Date'] = df2['date']
+        # df.columns = ['open', 'high', 'low', 'close', 'volume', 'date']
+        # df['RSI'] = RSI(df, 14)
+        # # MACD
+        # # df = MACD(df)
+        # df["MA_20"] = EMA(df, 20)
+        # df["ADX"] = ADX(df, 20)
+        # df["OBV"] = OBV(df)
+        # df["slope"] = slope(df)
+        
         # return json.dumps(json.loads(df.to_json(orient='records')), indent=2)
         return json.dumps({
             'success': True,
-            'data': json.loads(df.to_json(orient='records'))
+            'data': r_json['c']
         })
     except:
         return json.dumps({
